26b07a31b604caa3adad8d8796a9bd65a12529d4
George Fam <fam.george@courrier.uqam.ca>
2025-11-02 22:50:02 -0500

    Add unfinished test program (efficiency TODO)

--- testtp3.cpp
+++ testtp3.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
+#include <chrono>
 #include "arbreavl.h"
 
-// Cette fonction vérifie la présence des fonctionnalités demandées au TP3 (de base + nouvelles).
-// dans la classe générique ArbreAVL dans arbreavl.h.
-// Si ce test compile, c'est bon signe. Mais, ce test n'est pas exhaustif.
-// Le bon fonctionnement n'est pas vérifié.
-// L'efficacité temporelle n'est pas mesurée.
 void test00_compilation(){
    ArbreAVL<int> a1;
    ArbreAVL<int> a2(a1);
@@ -15,7 +11,6 @@ void test00_compilation(){
    a1.inserer(1);
    a2.vider();
    
-   // Nouvelles fonctionnalitées TP3
    int t = a1.taille();
    b = (a3==a2);
    
@@ -25,42 +20,186 @@ void test00_compilation(){
    a1-=a3;
    a2 = a3-a3;
 }
-
-
 bool test01(){
    ArbreAVL<int> a;
    a.inserer(1);
-   int t = a.taille();
-   bool r = t==1;
-   if(!r)
-       std::cout << "Mauvaise taille !" << std::endl;
+   bool r = a.taille()==1;
+   a.inserer(1);
+   r &= a.taille()==1;
+   if(!r) std::cout << "Mauvaise taille !"<<std::endl;
    return r;
 }
-
 bool test02(){
    ArbreAVL<int> a;
    a.inserer(10);
    a.inserer(20);
    if(!a.contient(10)){
-       std::cout << "10 doit être dans a !" << std::endl;
+       std::cout<<"10 doit être dans a !"<<std::endl;
        return false;
    }
    if(a.contient(15)){
-       std::cout << "15 ne doit pas être dans a !" << std::endl;
+       std::cout<<"15 ne doit pas être dans a !"<<std::endl;
        return false;
    }
    return true;
+}
+bool test03_union(){
+    ArbreAVL<int> a, b,c;
+    a.inserer(1);
+    a.inserer(2);
+    b.inserer(2);
+    b.inserer(3);
+    c=a+b;
+    if(!(c.contient(1) && c.contient(2) && c.contient(3))){
+        std::cout << "Erreur dans union, 3 manquant!" << std::endl;
+        return false;
+    }
+    return true;
+}
+bool test04_diff(){
+    ArbreAVL<int> a,b,c;
+    a.inserer(1);
+    a.inserer(2);
+    a.inserer(3);
+    b.inserer(-1);
+    b.inserer(3);
+    b.inserer(4);
+    c = a - b;
+    if(c.contient(3)||c.contient (-1)||!c.contient(1)||!c.contient(2)){
+        std::cout << "Erreur dans difference" << std::endl;
+        return false;
+    }
+    return true;
+}
+bool test05_plusEgal(){
+    ArbreAVL<int> a,b;
+    a.inserer(1);
+    b.inserer(2);
+    b.inserer(3);
+    a += b;
+    if(!(a.contient(1) && a.contient(2) && a.contient(3))){
+        std::cout << "Erreur dans union, 3 manquant!" << std::endl;
+        return false;
+    }
+    return true;
+}
+bool test06_egalite(){
+    ArbreAVL<int> a, b;
+    for(int i=1;i<=5;i++) a.inserer(i);
+    b.inserer(3);
+    b.inserer(1);
+    b.inserer(4);
+    b.inserer(2);
+    b.inserer(5);
+    bool q=!(a==b);
+    b.inserer(6);
+    bool r=(a==b);
+    if(q||r){
+        std::cout<< "Erreur dans operateur egalite" << 
+            "(evalue structure au lieu d'elements)"<< std::endl;
+        return false;
+    }
+    return true;
+}
+bool test07_taille_ops(){
+    ArbreAVL<int> a,b,c,d;
+    for(int i=1;i<=500;i++) a.inserer(i);
+    for(int i=251;i<=750;i++) b.inserer(i);
+
+    if(a.taille()!=500 || b.taille()!=500){
+        std::cout<<"Erreur taille initiale (a ou b .taille() doit etre 500)"  
+            <<std::endl;
+        return false;
+    }
+
+    c = a+b;
+    if(c.taille()!=750){
+        std::cout<<"Erreur taille + (1-500 + 251-750 doit etre 1-750)"
+            <<std::endl;
+        return false;
+    }
+
+    d = a;
+    d += b;
+    if(d.taille()!=750){
+        std::cout<<"Erreur taille += (1-500 + 251-750 doit etre 1-750)"
+            <<std::endl;
+        return false;
+    }
+
+    c = a-b;
+    if(c.taille()!=250){
+        std::cout<<"Erreur taille - (1-500 - 251-750 doit etre 1-250)"
+            <<std::endl;
+        return false;
+    }
+
+
+    d = a;
+    d -= b;
+    if(d.taille()!=250){
+        std::cout<<"Erreur taille -= (1-500 - 251-750 doit etre 1-250)"
+            <<std::endl;
+        return false;
+    }
+
+    return true;
+}
+bool test08_ops_identique(){
+    ArbreAVL<int> a,b,c;
+    bool r=false;
+    for(int i = 1; i <=20; i++) {
+        a.inserer(i);
+        b.inserer(i);
+    }
+    a+=a;
+    if(a.taille()!=20) r=true;
+    c = a + b;
+    if(c.taille()!=20) r=true;
+    c = b - a;
+    if(c.taille()!=0) r=true;
+    b-=a;
+    if(b.taille()!=0) r=true;
+    if(r){
+        std::cout<<"Erreur dans operations avec operateurs identiques"
+            <<std::endl;
+        return false;
+    }
+    return true;
+}
+bool test09_copie(){
+    ArbreAVL<int> a,b;
+    a.inserer(1);
+    a.inserer(2);
+    b = a;
+    ArbreAVL<int> c(b);
+    a.inserer(3);
+    b.inserer(4);
+    if(b.contient(3) || c.contient(4)){
+        std::cout<<"Erreur copie non profonde"<<std::endl;
+        return false;
+    }
+    return true;
 }
 
 int main(){
     test00_compilation();
     int nb_types_erreur=0;
-    if(!test01())
-        nb_types_erreur++;
-    if(!test02())
-        nb_types_erreur++;
-    
-    // Important: retourner 0 si aucune erreur. Sinon >0. Il est suggéré de retourner le nombre de types d'erreurs détectées.
+    if(!test01()) nb_types_erreur++;
+    if(!test02()) nb_types_erreur++;
+    if(!test03_union()) nb_types_erreur++;
+    if(!test04_diff()) nb_types_erreur++;
+    if(!test05_plusEgal()) nb_types_erreur++;
+    if(!test06_egalite()) nb_types_erreur++;
+    if(!test07_taille_ops()) nb_types_erreur++;
+    if(!test08_ops_identique()) nb_types_erreur++;
+    if(!test09_copie()) nb_types_erreur++;
+
+    // if(!testE_taille()) nb_types_erreur++;
+    // if(!testE_egal()) nb_types_erreur++;
+    // if(!testE_plusEgal()) nb_types_erreur++;
+    // if(!testE_plus()) nb_types_erreur++;
+    // if(!testE_moinsEgal()) nb_types_erreur++;
     return nb_types_erreur;
 }
 
